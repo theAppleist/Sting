493c462adc700d91949ac586da69fa5dbfed59f3
ldoron111 <ldoron111@gmail.com>
2015-11-13 20:52:56 +0200

    a

--- Sting/DAL/CommunicatorImplemenatations/InsertCommunicator.cs
+++ Sting/DAL/CommunicatorImplemenatations/InsertCommunicator.cs
@@ -1,11 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using DAL.Communicator;
 
 namespace DAL.CommunicatorImplemenatations
 {
-    
+    public class InsertCommunicator :IInsertCommuncitor
+    {
+        private readonly string InsertCommand = "INSERT INTO {0} OUTPUT INSERTED.ID VALUES {1}";  
+        private TableCommunicationParameters _parameters;
+
+        public InsertCommunicator(TableCommunicationParameters paramerters)
+        {
+            _parameters = paramerters;
+        }
+
+        public int Insert(object insertedObject)
+        {
+           
+        }
+
+        private string GenerateValuesString(PropertyInfo [] properties)
+        {
+            
+        }
+
+        private string GenerateKeysString(List<object> keys)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
