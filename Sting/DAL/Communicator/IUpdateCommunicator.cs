2c9915a00960d5682914308f5e6c467b2f756f65
theAppleist <sapirkraus@me.com>
2015-11-13 20:04:37 +0200

    Added Update Communicator

---
+++ Sting/DAL/Communicator/IUpdateCommunicator.cs
@@ -0,0 +1,14 @@
+﻿using DAL.Filter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Communicator
+{
+    public interface IUpdateCommunicator
+    {
+        bool Update(IDictionary<string, string> values, IFilter filter);
+    }
+}
