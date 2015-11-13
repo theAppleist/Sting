1b51e23e86aaf1433db815bc458b768b3e4c40c2
theAppleist <sapirkraus@me.com>
2015-11-13 19:31:17 +0200

    Added Filters

---
+++ Sting/DAL/Filter/Filter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Filter
+{
+    public class Filter : IFilter
+    {
+        public string Column { get; set; }
+        public string Value { get; set; }
+        public FilterComparer.Types Comparer { get; set; }
+
+        public Filter(string column, string value, FilterComparer.Types comparer)
+        {
+            Column = column;
+            Value = value;
+            Comparer = comparer;
+        }
+
+        public string GetFilterString()
+        {
+            return string.Format("({0}{1}`{2}`)", Column, FilterComparer.ConvertToString(Comparer), Value);
+        }
+
+        public override string ToString()
+        {
+            return GetFilterString();
+        }
+    }
+}
---
+++ Sting/DAL/Filter/FilterComparer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Filter
+{
+    public class FilterComparer
+    {
+        public enum Types
+        {
+            Equals,
+            Greater,
+            Lesser,
+            GreaterAndEquals,
+            LesserAndEquals,
+            NotEquals
+        }
+        public static string ConvertToString(FilterComparer.Types comparer)
+        {
+            switch (comparer)
+            {
+                case Types.Equals:
+                    return "=";
+                case Types.Greater:
+                    return ">";
+                case Types.Lesser:
+                    return "<";
+                case Types.GreaterAndEquals:
+                    return ">=";
+                case Types.LesserAndEquals:
+                    return "<=";
+                case Types.NotEquals:
+                    return "!=";
+            }
+            return null;
+        }
+    }
+
+
+    
+}
