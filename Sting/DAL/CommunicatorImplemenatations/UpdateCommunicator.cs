65986f9479c13de32e588b8d85c3f2b0bdcc729f
ldoron111 <ldoron111@gmail.com>
2015-11-13 21:49:13 +0200

    user

--- Sting/DAL/CommunicatorImplemenatations/UpdateCommunicator.cs
+++ Sting/DAL/CommunicatorImplemenatations/UpdateCommunicator.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace DAL.CommunicatorImplemenatations
 {
-    public class UpdateCommunicator : TableCommunicator : 
+    public class UpdateCommunicator  
     {
     }
 }
--- Sting/Sting/Controllers/UsersController.cs
+++ Sting/Sting/Controllers/UsersController.cs
@@ -1,26 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using DAL.Communicator;
 using DAL.CommunicatorImplemenatations;
+using DAL.Filter;
 using StingCore;
 
 namespace Sting.Controllers
 {
+    //later fill columns in table parameters
     public class UsersController : ApiController
     {
         public User Get(int id)
         {
+            var parameters = new TableCommunicationParameters("dbo.Users", ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString, new List<string>());
+            IReadCommunicator communicator = new ReadCommunicator(parameters,typeof(User));
+            return (User)communicator.GetRecords(new WhereFilter(new ComparisonFilter("Id", "" + id, FilterComparer.Types.Equals))).FirstOrDefault();
+
 
         }
 
         public void Post(User user)
         {
-            //var parameters = new TableCommunicationParameters("dbo.Users", );
-            //IInsertCommuncitor communcitor = new InsertCommunicator();
+            var parameters = new TableCommunicationParameters("dbo.Users",ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString,new List<string>{"RoleId,FirstName,LasstName"} );
+            IInsertCommuncitor communcitor = new InsertCommunicator(parameters);
+            var id =communcitor.Insert(user);
+            if (id == -1)
+            {
+                throw  new HttpRequestException("cant add user ");
+            }
         }
     }
 }
