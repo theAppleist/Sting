c6095cfbfaf1be5fb032f30867013dd8ee5e9191
theAppleist <sapirkraus@me.com>
2015-11-13 21:48:20 +0200

    Added SetFilter, CombinationFilter, UpdateCommunicator

--- Sting/DAL/CommunicatorImplemenatations/UpdateCommunicator.cs
+++ Sting/DAL/CommunicatorImplemenatations/UpdateCommunicator.cs
@@ -4,10 +4,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL.Filter;
+using System.Data.SqlClient;
 
 namespace DAL.CommunicatorImplemenatations
 {
-    public class UpdateCommunicator : TableCommunicator : 
+    public class UpdateCommunicator : TableCommunicator, IUpdateCommunicator
     {
+        public UpdateCommunicator(TableCommunicationParameters parameters)
+            :base(parameters)
+        {
+
+        }
+
+        public bool Update(IDictionary<string, string> values, params IFilter[] filters)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    string baseQuery = string.Format("UPDATE {0}");
+                    foreach (IFilter filter in filters)
+                    {
+                        baseQuery += string.Format(" {0}", filter.GetFilterString());
+                    }
+                    command.CommandText = baseQuery;
+
+                    using(SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+
+                        }
+                    }
+                }
+            }
+            return true;
+        }
     }
 }
---
+++ Sting/DAL/Filter/SetFilter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Filter
+{
+    public class SetFilter : IFilter
+    {
+        public IFilter Filter { get; set; }
+        public SetFilter(IFilter filter)
+        {
+            Filter = filter;
+        }
+
+        public string GetFilterString()
+        {
+            return string.Format("SET {0}", Filter);
+        }
+
+        public override string ToString()
+        {
+            return GetFilterString();
+        }
+    }
+}
