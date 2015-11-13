c3dc3891a2c99290fc10b9ef653955dd451ce535
ldoron111 <ldoron111@gmail.com>
2015-11-13 21:29:26 +0200

    a

--- Sting/Sting/Controllers/UsersController.cs
+++ Sting/Sting/Controllers/UsersController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using DAL.Communicator;
+using DAL.CommunicatorImplemenatations;
 using StingCore;
 
 namespace Sting.Controllers
@@ -12,12 +14,13 @@ namespace Sting.Controllers
     {
         public User Get(int id)
         {
-            throw new NotImplementedException();
+
         }
 
         public void Post(User user)
         {
-            throw new NotImplementedException();
+            //var parameters = new TableCommunicationParameters("dbo.Users", );
+            //IInsertCommuncitor communcitor = new InsertCommunicator();
         }
     }
 }
