125fe1b3257bc4935f1c62d26d8e9adaa5515360
theAppleist <sapirkraus@me.com>
2015-11-13 20:58:32 +0200

    Added table parameter - columns

--- Sting/DAL/Communicator/TableCommunicatorParameters.cs
+++ Sting/DAL/Communicator/TableCommunicatorParameters.cs
@@ -11,11 +11,13 @@ namespace DAL.Communicator
     {
         public string TableName { get; set; }
         public string ConnectionString { get; set; }
+        public IEnumerable<string> Columns { get; set; }
 
-        public TableCommunicationParameters(string tableName, string connectionString)
+        public TableCommunicationParameters(string tableName, string connectionString, IEnumerable<string> columns)
         {
             TableName = tableName;
             ConnectionString = connectionString;
+            Columns = columns;
         }
 
         
--- Sting/DAL/CommunicatorImplemenatations/ReadCommunicator.cs
+++ Sting/DAL/CommunicatorImplemenatations/ReadCommunicator.cs
@@ -4,15 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DAL.Communicator;
+using DAL.Filter;
 
 namespace DAL.CommunicatorImplemenatations
 {
-    public class ReadCommunicator : TableCommunicator
+    public class ReadCommunicator : TableCommunicator, IReadCommunicator
     {
         public ReadCommunicator(TableCommunicationParameters parameters)
             :base(parameters)
         {
 
+        }
+
+        public IEnumerable<string> GetColumns()
+        {
+            
+        }
+
+        public Type GetCommunicatorModelType()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<object> GetRecords(IFilter filter)
+        {
+            throw new NotImplementedException();
         }
     }
 }
