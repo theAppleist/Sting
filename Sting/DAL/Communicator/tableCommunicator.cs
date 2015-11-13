a7128bd69fa4ab43b60a2eaef4979c9a98ec462f
ldoron111 <ldoron111@gmail.com>
2015-11-13 20:42:57 +0200

    dal

---
+++ Sting/DAL/Communicator/tableCommunicator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Communicator
+{
+    public  class TableCommunicationparameters
+    {
+        public string TableName { get; set; }
+        public string ConnectionString { get; set; }
+
+        public TableCommunicationparameters(string tableName, string connectionString)
+        {
+            TableName = tableName;
+            ConnectionString = connectionString;
+        }
+
+        
+    }
+}
